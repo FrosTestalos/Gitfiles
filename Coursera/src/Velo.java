ff8ba602ed39e52618c48c3aaf320edb703a063d
@Gauthier <Gauthier.vig@gmail.com>
2015-09-30 10:37:03 +0200

    adding class Champi

--- Coursera/src/Velo.java
+++ Coursera/src/Velo.java
@@ -12,13 +12,34 @@ public class Velo {
         /*******************************************
          * Completez le programme a partir d'ici.
          *******************************************/
+        if ((debut>24)||(fin>24)||(debut<0)||(fin<0)){
+            System.out.println("Les heures doivent être comprises entre 0 et 24 !");
+        }else if (debut==fin) {
+            System.out.println("Bizarre, vous n'avez pas loué votre vélo bien longtemps !");
+        }else if (debut>fin){
+            System.out.println("Bizarre, le début de la location est après la fin ...");
+        }else {int duree=fin-debut;
+            int total1=0;
+            int total2=0;
+            int total =0;
 
-        System.out.println("Les heures doivent être comprises entre 0 et 24 !");
-        System.out.println("Bizarre, vous n'avez pas loué votre vélo bien longtemps !");
-        System.out.println("Bizarre, le début de la location est après la fin ...");
-        System.out.println("Vous avez loué votre vélo pendant");
-        System.out.print("Le montant total à payer est de ");
-        System.out.println(" francs.");
+            for(int i=1;i<=duree;i++){
+                if (((debut>=0)&&(debut<7))||((debut>=17)&&(debut<24))) {
+                    total1+=1;
+                }else {total2+=2;}
+                debut++;
+                total=total1+total2;
+
+            }
+            System.out.println("Vous avez loué votre vélo pendant ");
+            if (total1>0)System.out.println(total1+" heure(s) au tarif horaire de 1.0 francs");
+            if (total2>0){System.out.println(total2/2+" heure(s) au tarif horaire de 2.0 francs");}
+            System.out.print("Le montant total à payer est de ");
+            System.out.print(total);
+            System.out.println(" francs.");
+
+        }
+
 
         /*******************************************
          * Ne rien modifier apres cette ligne.
