b86f2a916493119abe6ded13b9811cc9b37f37cc
Isep_Gauthier <Gauthier.vig@gmail.com>
2015-09-30 19:53:29 +0200

    px fixe test 1

--- Coursera/src/Velo.java
+++ Coursera/src/Velo.java
@@ -12,24 +12,23 @@ public class Velo {
         /*******************************************
          * Completez le programme a partir d'ici.
          *******************************************/
+        int total1=0;
+        int total2=0;
+        double total =0.0;
+        int duree=fin-debut;
         if ((debut>24)||(fin>24)||(debut<0)||(fin<0)){
             System.out.println("Les heures doivent être comprises entre 0 et 24 !");
         }else if (debut==fin) {
             System.out.println("Bizarre, vous n'avez pas loué votre vélo bien longtemps !");
         }else if (debut>fin){
             System.out.println("Bizarre, le début de la location est après la fin ...");
-        }else {int duree=fin-debut;
-            int total1=0;
-            int total2=0;
-            int total =0;
-
+        }else {
             for(int i=1;i<=duree;i++){
                 if (((debut>=0)&&(debut<7))||((debut>=17)&&(debut<24))) {
                     total1+=1;
                 }else {total2+=2;}
                 debut++;
                 total=total1+total2;
-
             }
             System.out.println("Vous avez loué votre vélo pendant ");
             if (total1>0)System.out.println(total1+" heure(s) au tarif horaire de 1.0 francs");
@@ -37,10 +36,7 @@ public class Velo {
             System.out.print("Le montant total à payer est de ");
             System.out.print(total);
             System.out.println(" francs.");
-
         }
-
-
         /*******************************************
          * Ne rien modifier apres cette ligne.
          *******************************************/
